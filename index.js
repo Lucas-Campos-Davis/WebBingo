60d73a9541d84c9d190536932c28981ee784b2b1
Lucas Campos-Davis <camposdl@oregonstate.edu>
2019-12-10 14:14:06 -0800

    Added a print button that only renders the card for printing

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ function formatWord(word){
 }
 
 
+function printCard(){
+    var prtContent = document.getElementById("cardContainer");
+    var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+    WinPrint.document.write('<link rel="stylesheet" type="text/css" href="style.css" />');
+    WinPrint.document.write(prtContent.innerHTML);
+    WinPrint.document.close();
+    WinPrint.setTimeout(function(){
+        WinPrint.focus();
+        WinPrint.print();
+        WinPrint.close();
+      }, 1000);
+}
+
+
 window.addEventListener('DOMContentLoaded', function () {
 
     var AddWordsButton = document.getElementById('addWordsButton');
@@ -69,6 +83,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
     var RemoveWordsButton = document.getElementById('removeWordsButton');
     RemoveWordsButton.addEventListener('click', importWordsToRemoveAndRemove);
+
+    var PrintButton = document.getElementById('printCardButton');
+    PrintButton.addEventListener('click', printCard);
 
 });
 
